970a188a966de0b14a7c93728947e581fa9cf49f
adrimartin6 <adrianmartincalvet@gmail.com>
2024-10-17 20:03:53 +0200

    Primera versión del css

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <h2 class="title"> Figuras Geometricas </h2>
+    <div class="container-cuadrado">
+        <input type="button" class="add-cuadrado">
+    </div>
+
+    <div class="container-triangulo">
+        <input type="button" class="add-triangulo">
+    </div>
+
+    <div class="container-circulo">
+        <input type="button" class="add-circulo">
+    </div>
+
+    <div class="container-rectangulo">
+        <input type="button" class="add-rectangulo">
+    </div>
+</div>
+
+</body>
+</html>
