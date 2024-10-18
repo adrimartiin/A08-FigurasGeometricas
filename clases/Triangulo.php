59301cd1af053c1d4c654134f6eeee16cbdb5e23
adrimartin6 <adrianmartincalvet@gmail.com>
2024-10-18 17:11:20 +0200

    Archivo de la clase triangulo hecho

---
+++ clases/Triangulo.php
@@ -0,0 +1,31 @@
+<?php
+
+class Triangulo extends FigurasGeometricas {
+    public $lado2;
+    public $lado3;
+
+    public function __construct($tipoFigura, $lado1, $lado2, $lado3) {
+        parent::__construct($tipoFigura, $lado1);
+        $this->lado2 = $lado2;
+        $this->lado3 = $lado3;
+    }
+
+    public function setLados($lado2, $lado3){
+        $this->lado2 = $lado2;
+        $this->lado3 = $lado3;
+    }
+
+    public function getLados(){
+        return $this->lado2 . " " . $this->lado3;
+    }
+
+    public function calculaAreaTriangulo(){
+        $area = $this->lado1 * $this->lado2 / 2;
+        return $area;
+    }
+
+    public function calculaPerimetroTriangulo(){
+        $perimetro = $this->lado1 + $this->lado2 + $this->lado3;
+        return $perimetro;
+    }
+}
