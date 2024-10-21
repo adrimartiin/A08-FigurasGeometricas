df759e9c7310cc5afb4577866e648468fee543a3
adrimartin6 <adrianmartincalvet@gmail.com>
2024-10-21 10:34:31 +0200

    Index actualizado

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/styles.css">
-    <title>Document</title>
+    <title>Figuras Geométricas</title>
 </head>
 <body>
 
@@ -13,31 +13,38 @@
         <h2 class="fira-sans-condensed-extralight">Figuras Geométricas</h2>
     </div>
     
-    <form action = "./proc/procFigs.php" method="post">
-    <div class="inputs-container">
-        
-        <div class="container-cuadrado">
-            <input type="button" name="figura" class="add-cuadrado">
-        </div>
+    <form action="./proc/procFigs.php" method="post">
+        <div class="inputs-container">
+            
+            <div class="container-cuadrado">
+                <button type="submit" name="figura" value="cuadrado" class="add-cuadrado">
+                    <img src="./img/img_cuadrado.png">
+                </button>
+            </div>
+            
+            
+            <div class="container-triangulo">
+                <button type="submit" name="figura" value="triangulo" class="add-triangulo">
+                    <img src="./img/img_triangulo.png">
+                </button>
+            </div>
 
-        <div class="container-triangulo">
-            <input type="button" name="figura"  class="add-triangulo">
-        </div>
+            
+            <div class="container-circulo">
+                <button type="submit" name="figura" value="circulo" class="add-circulo">
+                    <img src="./img/img_circulo.png">
+                </button>
+            </div>
 
-        <div class="container-circulo">
-            <input type="button" name="figura" class="add-circulo">
+            
+            <div class="container-rectangulo">
+                <button type="submit" name="figura" value="rectangulo" class="add-rectangulo">
+                    <img src="./img/img_rectangulo.png">
+                </button>
+            </div>
         </div>
-
-        <div class="container-rectangulo">
-            <input type="button" name="figura"  class="add-rectangulo">
-        </div>
-    </div>
     </form>
-    
-
 </div>
-
-
 
 </body>
 </html>
