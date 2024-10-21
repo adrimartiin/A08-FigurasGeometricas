a62b611ef89241f1469842dfefbc92c8c3f07abe
adrimartin6 <adrianmartincalvet@gmail.com>
2024-10-21 17:50:08 +0200

    Archivo de procesoFiguras actualizado

--- proc/procFigs.php
+++ proc/procFigs.php
@@ -9,6 +9,7 @@
     <div class="container">
         
         <div class="result-container">
+            <form action="../resultados.php" method="post">
             <?php
                 if(isset($_POST['figura'])){
                     $figuraSeleccionada = $_POST['figura'];
@@ -17,25 +18,29 @@
                     
                     switch ($figuraSeleccionada){
                         case 'cuadrado':
-                            echo "<input type='number' name='lado' placeholder='Introduce el lado'><br><br>";
+                            echo "<input type='hidden' name='figura' value='cuadrado'>";
+                            echo "<input type='number' name='lado' step='0.01' placeholder='Introduce el lado'><br><br>";
                             echo "<input type='submit' value='Calcular'>";
                         break;
 
                         case 'triangulo': 
-                            echo "<input type='number' name='lado1' placeholder='Introduce el primer lado'><br>";
-                            echo "<input type='number' name='lado2' placeholder='Introduce el segundo lado'><br>";
-                            echo "<input type='number' name='lado3' placeholder='Introduce el tercer lado'><br>";
+                            echo "<input type='hidden' name='figura' value='triangulo'>";
+                            echo "<input type='number' name='lado1' step='0.01' placeholder='Introduce el primer lado'><br>";
+                            echo "<input type='number' name='lado2' step='0.01' placeholder='Introduce el segundo lado'><br>";
+                            echo "<input type='number' name='lado3' step='0.01' placeholder='Introduce el tercer lado'><br>";
                             echo "<input type='submit' value='Calcular'>";
                         break;
 
                         case 'circulo':
-                            echo "<input type='number' name='radio' placeholder='Introduce el radio del circulo'><br><br>";
+                            echo "<input type='hidden' name='figura' value='circulo'>";
+                            echo "<input type='number' name='radio' step='0.01' placeholder='Introduce el radio del circulo'><br><br>";
                             echo "<input type='submit' value='Calcular'>";
                         break;
 
                         case 'rectangulo':
-                            echo "<input type='number' name='base' placeholder='Introduce la base del rectangulo'><br><br>";
-                            echo "<input type='number' name='altura' placeholder='Introduce la altura del rectangulo'><br><br>";
+                            echo "<input type='hidden' name='figura' value='rectangulo'>";
+                            echo "<input type='number' name='base' step='0.01' placeholder='Introduce la base del rectangulo'><br><br>";
+                            echo "<input type='number' name='altura' step='0.01' placeholder='Introduce la altura del rectangulo'><br><br>";
                             echo "<input type='submit' value='Calcular'>";
                         break;
                     }
@@ -44,6 +49,7 @@
                     echo "<p>No se ha seleccionado ninguna figura</p>";
                 }
             ?>
+            </form>
         </div> 
     </div> 
 </body>
