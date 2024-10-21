429b32271f53436d1a97d88ce21e9f60a5b555b2
adrimartin6 <adrianmartincalvet@gmail.com>
2024-10-21 10:34:16 +0200

    Página intermedia php hecho, falta validaciones JS

--- proc/procFigs.php
+++ proc/procFigs.php
@@ -1,33 +1,50 @@
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-   
-    $figuraSeleccionada = $_POST['figura'];
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Figura Seleccionada</title>
+</head>
+<body>
+    <div class="container">
+        
+        <div class="result-container">
+            <?php
+                if(isset($_POST['figura'])){
+                    $figuraSeleccionada = $_POST['figura'];
+                    echo "<h2 class='figure-title'> Figura Seleccionada: ".  ucfirst($figuraSeleccionada) ."</h2>";
+                    echo "<div class='input-group'>";
+                    
+                    switch ($figuraSeleccionada){
+                        case 'cuadrado':
+                            echo "<input type='number' name='lado' placeholder='Introduce el lado'><br><br>";
+                            echo "<input type='submit' value='Calcular'>";
+                        break;
 
-    echo "<h2> Figura Seleccionada: $figuraSeleccionada </h2>";
+                        case 'triangulo': 
+                            echo "<input type='number' name='lado1' placeholder='Introduce el primer lado'><br>";
+                            echo "<input type='number' name='lado2' placeholder='Introduce el segundo lado'><br>";
+                            echo "<input type='number' name='lado3' placeholder='Introduce el tercer lado'><br>";
+                            echo "<input type='submit' value='Calcular'>";
+                        break;
 
-    switch ($figuraSeleccionada){
-        case 'Cuadrado':
-            echo "<label>" . $figuraSeleccionada . "</label>";
-            echo "<input type'number' name='lado' placeholder='Introduce el lado'>";
-        break;
+                        case 'circulo':
+                            echo "<input type='number' name='radio' placeholder='Introduce el radio del circulo'><br><br>";
+                            echo "<input type='submit' value='Calcular'>";
+                        break;
 
-        case 'Triangulo':
-            echo "<label>" . $figuraSeleccionada . "</label>";
-            echo "<input type'number' name='lado1' placeholder='Introduce el primer lado'>";
-            echo "<input type'number' name='lado2' placeholder='Introduce el segundo lado'>";
-            echo "<input type='number' name='lado3' placeholder='Introduce el tercer lado'>";
-        break;
-
-        case 'Circulo':
-            echo "<label>" . $figuraSeleccionada . "</label>";
-            echo "<input type'number' name='radio' placeholder='Introduce el radio del circulo'>";
-        break;
-
-        case 'Rectangulo':
-            echo "<label>". $figuraSeleccionada. "</label>";
-            echo "<input type'number' name='base' placeholder='Introduce la base del rectangulo'>";
-            echo "<input type'number' name='altura' placeholder='Introduce la altura del rectangulo'>";
-        break;
-
-    }
-}
+                        case 'rectangulo':
+                            echo "<input type='number' name='base' placeholder='Introduce la base del rectangulo'><br><br>";
+                            echo "<input type='number' name='altura' placeholder='Introduce la altura del rectangulo'><br><br>";
+                            echo "<input type='submit' value='Calcular'>";
+                        break;
+                    }
+                    echo "</div>"; 
+                } else {
+                    echo "<p>No se ha seleccionado ninguna figura</p>";
+                }
+            ?>
+        </div> 
+    </div> 
+</body>
+</html>
