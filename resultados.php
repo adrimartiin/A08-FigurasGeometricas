7498ec9c1d2148d74a9712bd5cd8d00e0c088faf
adrimartin6 <adrianmartincalvet@gmail.com>
2024-10-23 19:27:20 +0200

    Resultados.php actualizado

--- resultados.php
+++ resultados.php
@@ -1,4 +1,9 @@
-
+<?php
+    session_start();
+    if(!isset($_SESSION['figura'])){
+        header("Location:./index.php?error=2");
+    } 
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,42 +15,66 @@
     <div class="container">
         <?php
             
-            if(isset($_POST['figura'])){
-                $figura = $_POST['figura'];
+            if(isset($_SESSION['figura'])){
+                $figura = $_SESSION['figura'];
                 echo "<h2 class='figure-title'>Cálculo de área y perímetro de la figura:" . ucfirst($figura) . "</h2>";
                 switch ($figura){
                     case 'cuadrado':
-                        $lado = $_POST['lado'];
+                        if(isset($_SESSION['lado'])){
+                        $lado = $_SESSION['lado'];
                         include_once './clases/Cuadrado.php';
                         $cuadrado = new Cuadrado($lado);
                         echo "<p>Área: ". $cuadrado->calculaAreaCuadrado(). "</p>";
                         echo "<p>Perímetro: ". $cuadrado->calculaPerimetroCuadrado(). "</p>";
+                        echo "<a href='./proc/procCierraSesion.php' class='links'>Volver a inicio</a><br>";
+                        echo "<a href='./proc/procFigs.php' class='links'>Volver a introducir datos</a><br>";
+                        } else {
+                            echo "<p>No se ha proporcionado el lado del cuadrado.</p>";
+                        }
                         break;
                     
                     case 'triangulo':
-                        $base = $_POST['lado1'];
-                        $altura = $_POST['lado2'];
-                        $numParaPerimetro = $_POST['lado3'];
+                        if (isset($_SESSION['lado1'], $_SESSION['lado2'], $_SESSION['lado3'])) {
+                        $base = $_SESSION['lado1'];
+                        $altura = $_SESSION['lado2'];
+                        $numParaPerimetro = $_SESSION['lado3'];
                         include_once './clases/Triangulo.php';
                         $triangulo = new Triangulo($figura, $base, $altura, $numParaPerimetro);
                         echo "<p>Área: ". $triangulo->calculaAreaTriangulo(). "</p>";
                         echo "<p>Perímetro: ". $triangulo->calculaPerimetroTriangulo(). "</p>";
+                        echo "<a href='./proc/procCierraSesion.php' class='links'>Volver a inicio</a><br>";
+                        echo "<a href='./proc/procFigs.php' class='links'>Volver a introducir datos</a><br>";
+                        } else {
+                            echo "<p>No se han proporcionado los lados del triángulo.</p>";
+                        }
                         break;
                     
                     case 'circulo':
-                        $radio = $_POST['radio'];
+                        if(isset($_SESSION['radio'])){
+                        $radio = $_SESSION['radio'];
                         include_once './clases/Circulo.php';
                         $circulo = new Circulo($radio);
                         echo "<p>Área: ". round($circulo->calcularAreaCirculo(), 2). "</p>";
                         echo "<p>Perímetro: ". round($circulo->calcularPerimetroCirculo(), 2). "</p>";
+                        echo "<a href='./proc/procCierraSesion.php' class='links'>Volver a inicio</a><br>";
+                        echo "<a href='./proc/procFigs.php' class='links'>Volver a introducir datos</a><br>";
+                        } else {
+                            echo "<p>No se ha proporcionado el radio del círculo.</p>";
+                        }
                         break;
                     case 'rectangulo':
-                        $baseRect = $_POST['base'];
-                        $alturaRect = $_POST['altura'];
+                        if(isset($_SESSION['base'], $_SESSION['altura'])){
+                        $baseRect = $_SESSION['base'];
+                        $alturaRect = $_SESSION['altura'];
                         include_once './clases/Rectangulo.php';
                         $rectangulo = new Rectangulo($figura, $baseRect, $alturaRect);
                         echo "<p>Área: ". $rectangulo->calcularAreaRectangulo(). "</p>";
                         echo "<p>Perímetro: ". $rectangulo->calcularPerimetroRectangulo(). "</p>";
+                        echo "<a href='./proc/procCierraSesion.php' class='links'>Volver a inicio</a><br>";
+                        echo "<a href='./proc/procFigs.php' class='links'>Volver a introducir datos</a><br>";
+                        } else {
+                            echo "<p>No se han proporcionado los lados del rectángulo.</p>";
+                        }
                         break;
                 }
             } else {
